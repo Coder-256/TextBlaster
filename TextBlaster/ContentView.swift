26057ec2e48135c355bea994076d1315e71f9de8
Jacob Greenfield <jacob@jacobgreenfield.me>
2023-08-26 13:55:55 -0400

    Initial commit

---
+++ TextBlaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextBlaster
+//
+//  Created by Jacob Greenfield on 8/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextBlaster/TextBlasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextBlasterApp.swift
+//  TextBlaster
+//
+//  Created by Jacob Greenfield on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TextBlasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
